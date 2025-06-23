575b4dc2e27d203ec17b3bf7556522bf9ca27237
jose <jose.pobletealumnos.usm.cl>
2025-06-23 02:25:55 -0400

    FIX:fix app_infra tf

---
+++ terraform/modules/app_infra/variables.tf
@@ -0,0 +1,16 @@
+# Variables definidas en el módulo
+
+variable "project_id" {}
+variable "region" {}
+variable "env" {}
+
+variable "app_image" {}
+variable "cpu" {}
+variable "memory" {}
+
+variable "db_user" {}
+variable "db_pass" {}
+variable "db_name" {}
+
+variable "openai_token" {}
+variable "api_token" {}
